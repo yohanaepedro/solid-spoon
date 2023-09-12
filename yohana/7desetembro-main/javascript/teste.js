840fd1118f87f59cf0822672349f1a4346317654
yohana <128655668+yohanaepedro@users.noreply.github.com>
2023-09-12 20:47:23 -0300

    Add files via upload

---
+++ yohana/7desetembro-main/javascript/teste.js
@@ -0,0 +1,10 @@
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+document.getElementById("demo").style.fontSize = "70px";
+document.getElementById("demo").style.display = "block";
+document.getElementById("demo").style.display = "none";
+
+
+document.getElementById("mano").style.fontSize = "70px";
+document.getElementById("mano").style.display = "block";
+document.getElementById("mano").style.display = "none";
